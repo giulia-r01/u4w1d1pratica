d08e2515f5c802b4d69a74813dab88ce44279189
Giulia Rizzo <girzzo@gmail.com>
2025-05-05 16:43:02 +0200

    Update Esercizio3.java

--- src/Esercizio3.java
+++ src/Esercizio3.java
@@ -4,21 +4,21 @@ public class Esercizio3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//        System.out.println("Scrivi il valore del primo lato:");
-//        double l1 = scanner.nextDouble();
-//
-//        System.out.println("Scrivi il valore del secondo lato:");
-//        double l2 = scanner.nextDouble();
-//
-//        double perimetro = perimetroRettangolo(l1, l2);
-//        System.out.println("Il perimetro del rettangolo è: " + perimetro);
-//
-//        System.out.println("Scrivi un numero");
-//        int n1 = scanner.nextInt();
-//
-//        String risultato = pariDispari(n1);
-//        System.out.println(risultato);
-//
+        System.out.println("Scrivi il valore del primo lato:");
+        double l1 = scanner.nextDouble();
+
+        System.out.println("Scrivi il valore del secondo lato:");
+        double l2 = scanner.nextDouble();
+
+        double perimetro = perimetroRettangolo(l1, l2);
+        System.out.println("Il perimetro del rettangolo è: " + perimetro);
+
+        System.out.println("Scrivi un numero");
+        int n1 = scanner.nextInt();
+
+        String risultato = pariDispari(n1);
+        System.out.println(risultato);
+
         System.out.println("Inserisci il valore del primo lato");
         double a = scanner.nextDouble();
         System.out.println("Inserisci il valore del secondo lato");
